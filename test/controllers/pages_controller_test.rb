1bdd78f28adeea922d3d6ad833349637129cbb9e
Roberto Rodriguez <robertorodriguezf@hotmail.com>
2016-03-22 02:57:28 +0000

    Pagina inicio

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get login" do
+    get :login
+    assert_response :success
+  end
+
+  test "should get bucar" do
+    get :bucar
+    assert_response :success
+  end
+
+end
